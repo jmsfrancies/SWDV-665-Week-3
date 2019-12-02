8b129c74c6b3c479c9ddb20b90c3c0410d16cada
James Francies <jfrancies1@live.maryville.edu>
2019-12-02 13:33:35 -0600

    Grocery Store

--- GroceryStore/src/pages/home/home.ts
+++ GroceryStore/src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,81 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  title = "Grocery";
+
+  items = [
+    {
+      name: "Milk",
+      quantity: 2    
+    },
+    {
+      name: "Bread",
+      quantity: 1    
+    },
+    {
+      name: "Banana",
+      quantity: 3    
+    },
+    {
+      name: "Sugar",
+      quantity: 1    
+    },
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
 
   }
 
+  removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+
+    this.items.splice(index, 1);
+  }
+
+  addItem() {
+    console.log("Adding Item");
+    this.showAddItemPrompt();
+  }
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
 }
+
+
+
